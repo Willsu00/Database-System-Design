167d409249223ec9895041c40de376ac30a32da4
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2023-10-19 15:17:41 +1300

    add table staff and projects

--- Assignment1.sql
+++ Assignment1.sql
@@ -37,10 +37,38 @@ DESC contracts;
 
 
 CREATE TABLE projects (
-    project_code NUMBER(4),
-    project_name VARCHAR(50),
+    project_code_PK NUMBER(4) NOT NULL,
+    contract_number_FK NUMBER(4),
+    project_name VARCHAR(50) NOT NULL,
     project_desc VARCHAR(50),
     project_start_date DATE,
-    project_end_date DATE
-    
+    project_end_date DATE,
+    CONSTRAINT project_code_primary PRIMARY KEY(project_code_PK),
+    CONSTRAINT contract_number_foreign FOREIGN KEY(contract_number_FK)
+        REFERENCES contracts (contract_number_PK)
 );
+
+DESC projects;
+
+CREATE TABLE staff (
+    employee_id_PK NUMBER(4) NOT NULL,
+    project_code_FK NUMBER(4) NOT NULL,
+    cc_name_FK VARCHAR2(50) NOT NULL,
+    employee_fname VARCHAR2(50) NOT NULL,
+    employee_lname VARCHAR2(50) NOT NULL,
+    employee_employment_date DATE,
+    employee_DOB DATE,
+    employee_gender CHAR(1),
+    employee_streetname VARCHAR2(50),
+    employee_streetno NUMBER(4),
+    employee_city VARCHAR2(50),
+    employee_phone NUMBER(10),
+    employee_email VARCHAR2(50),
+    CONSTRAINT employee_id_primary PRIMARY KEY (employee_id_PK),
+    CONSTRAINT project_code_foreign FOREIGN KEY (project_code_FK)
+        REFERENCES projects (project_code_PK),
+    CONSTRAINT cc_name_project_foreign FOREIGN KEY (cc_name_FK)
+        REFERENCES contractor_companies (cc_name_PK)
+);
+
+DESC staff;
